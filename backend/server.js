65040b15ece90433b37198008d65775476bc4e99
Jagrit Vats <69034224+jagritvats@users.noreply.github.com>
2022-09-19 00:39:39 +0530

    prod

--- backend/server.js
+++ backend/server.js
@@ -23,19 +23,19 @@ app.get('/api', (req, res) => {
 
 app.use('/api/face', faceRouter);
 
-// if (process.env.NODE_ENV == 'production') {
-// 	app.use(express.static(path.join(__dirname, '../frontend/build')));
+if (process.env.NODE_ENV == 'production') {
+	app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-app.get('*', (req, res) =>
-	res.sendFile(
-		path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
-	)
-);
-// } else {
-// 	app.get('*', (req, res) => {
-// 		res.send('Please set to production');
-// 	});
-// }
+	app.get('*', (req, res) =>
+		res.sendFile(
+			path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+		)
+	);
+} else {
+	app.get('*', (req, res) => {
+		res.send('Please set to production');
+	});
+}
 app.use('*', (req, res) => {
 	res.json({
 		success: false,
